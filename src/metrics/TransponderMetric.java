965e91694e15ce84200befef0852305f60dea916
aokita <shiamatsu@gmail.com>
2017-09-25 10:08:49 -0700

    added custom requests (alpha)

--- src/metrics/TransponderMetric.java
+++ src/metrics/TransponderMetric.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import models.Pair;
 import simulator.Simulator;
+import models.CustomRequest;
+import utilities.RequestsUtil;
 import utilities.NetworkTopology;
 
 public class TransponderMetric{
@@ -22,7 +25,7 @@ public class TransponderMetric{
 	public void start() throws IOException{;
 		//getResultsSimple(BANDWIDTH_DISTRIBUTION.RANDOM,EMBEDDING_METHOD.BACKUP);
 		//getResultsNSFNET(BANDWIDTH_DISTRIBUTION.RANDOM,EMBEDDING_METHOD.BACKUP);
-		getResults(BANDWIDTH_DISTRIBUTION.RANDOM,EMBEDDING_METHOD.BACKUP,NetworkTopology.RING8,30);
+		getResults(BANDWIDTH_DISTRIBUTION.RANDOM,EMBEDDING_METHOD.BACKUP,NetworkTopology.RING8,30,false);
 	//	getResults(BANDWIDTH_DISTRIBUTION.RANDOM,EMBEDDING_METHOD.BACKUP,NetworkTopology.HYPERCUBE8,40);
 	//	getResults(BANDWIDTH_DISTRIBUTION.RANDOM,EMBEDDING_METHOD.BACKUP,NetworkTopology.MESH8,50);
 	//	getResults(BANDWIDTH_DISTRIBUTION.RANDOM,EMBEDDING_METHOD.BACKUP,NetworkTopology.HYPERCUBE16,60);
@@ -33,45 +36,7 @@ public class TransponderMetric{
 		//getResultsHypercube16(BANDWIDTH_DISTRIBUTION.RANDOM,EMBEDDING_METHOD.BACKUP);
 	}
 	
-	public void getResultsSimple(BANDWIDTH_DISTRIBUTION distributionType, EMBEDDING_METHOD method) throws IOException{
-		File file = new File(PROJECT_DIRECTORY + RESULTS_DIRECTORY + "simple_transponder_new_4node_"+ distributionType.name().toLowerCase() + "_" + method.name().toLowerCase() + ".csv");
-		PrintWriter pw = new PrintWriter(file);
-		
-		pw.println("Max_Bandwidth,ODU,OPT,# ODU better than OPT ");
-
-		for(int i = 20; i <= 500; i+=20){
-			System.out.println("Starting Transponder Metric with max bandwidth: " + i);
-			int oduWins = 0;
-			
-			int sum1 = 0;
-			int sum2 = 0;
-			for(int j = 0; j < 1000; j++){
-				Simulator simulator = new Simulator(NetworkTopology.SIMPLE,Integer.MAX_VALUE, Integer.MAX_VALUE);
-				//simulator.setNumberOfRequest(500);
-				simulator.setMaxNodes(0);// setting requests with only two nodes.
-				simulator.setNumberOfRequest(6);
-				simulator.generateRequests();
-				//simulator.generateSpecificRequests();
-				simulator.setRequests();
-				
-				int oduTransponders = simulator.getTranspondersODU(100,i, distributionType.name().toLowerCase(), 0, (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true);
-				int optTransponders = simulator.getTransponderOPT(100,i, distributionType.name().toLowerCase(), (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true);
-				
-				if(oduTransponders < optTransponders) oduWins++;
-				
-				sum1 += oduTransponders; 
-				sum2 += optTransponders;
-			}
-			Pair<Integer,Integer> results = new Pair<Integer, Integer>(sum1/1000,sum2/1000);			
-			pw.println(i + "," + results.first() + "," + results.second() + "," + oduWins);
-		}
-		
-		pw.close();
-		
-		System.out.println("********************** done *******************");
-	}
-	
-	public void getResults(BANDWIDTH_DISTRIBUTION distributionType, EMBEDDING_METHOD method, NetworkTopology topology, int threshold) throws IOException{
+	public void getResults(BANDWIDTH_DISTRIBUTION distributionType, EMBEDDING_METHOD method, NetworkTopology topology, int threshold, boolean customRequest) throws IOException{
 		String fileName = "";
 		switch(topology) {
 			case SIMPLE:
@@ -139,6 +104,13 @@ public class TransponderMetric{
 			pw.println(i + "," + sum1/1000 + "," + sum2/1000 + "," + sum3/1000 + "," + oduWins);
 		}*/
 		
+		ArrayList<CustomRequest> requests = new ArrayList<CustomRequest>();
+		if(customRequest) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter location of file.");
+			requests = RequestsUtil.createCustomRequests(sc.nextLine());
+			sc.close();
+		}
 		for(int i = 20; i <= 500; i+=20){
 			System.out.println("Starting Transponder Metric with max bandwidth: " + i);
 			
@@ -153,9 +125,9 @@ public class TransponderMetric{
 				//simulator.generateSpecificRequests();
 				//simulator.setRequests();
 				
-				int SPFTransponders = simulator.getTranspondersODU(100,i, distributionType.name().toLowerCase(), 0, (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true);
-				int LUFTransponders = simulator.getTranspondersODU(100,i, distributionType.name().toLowerCase(), 1, (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true);
-				int MUFTransponders = simulator.getTranspondersODU(100,i, distributionType.name().toLowerCase(), 2, (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true);
+				int SPFTransponders = simulator.getTranspondersODU(100,i, distributionType.name().toLowerCase(), 0, (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true, requests);
+				int LUFTransponders = simulator.getTranspondersODU(100,i, distributionType.name().toLowerCase(), 1, (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true, requests);
+				int MUFTransponders = simulator.getTranspondersODU(100,i, distributionType.name().toLowerCase(), 2, (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true, requests);
 				
 				
 				
@@ -190,7 +162,7 @@ public class TransponderMetric{
 		System.out.println("********************** done *******************");
 	}
 	
-	public static void getResultsTest(BANDWIDTH_DISTRIBUTION distributionType, EMBEDDING_METHOD method, NetworkTopology topology, int threshold) throws IOException{
+	public static void getResultsTest(BANDWIDTH_DISTRIBUTION distributionType, EMBEDDING_METHOD method, NetworkTopology topology, int threshold, boolean customRequest) throws IOException{
 		String PROJECT_DIRECTORY =  new File(".").getCanonicalPath();
 		String RESULTS_DIRECTORY = "/src/results/";
 		String fileName = "";
@@ -223,6 +195,13 @@ public class TransponderMetric{
 		
 		pw.println("Max_Bandwidth,SPF,LUF,MUF,OPT,Hybrid,MUX ");
 		
+		ArrayList<CustomRequest> requests = new ArrayList<CustomRequest>();
+		if(customRequest) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter location of file.");
+			requests = RequestsUtil.createCustomRequests(sc.nextLine());
+			sc.close();
+		}
 		
 		//default 500
 		for(int i = 20; i <= 60; i+=20){
@@ -242,9 +221,9 @@ public class TransponderMetric{
 				//simulator.generateSpecificRequests();
 				//simulator.setRequests();
 				
-				int SPFTransponders = simulator.getTranspondersODU(100,i, distributionType.name().toLowerCase(), 0, (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true);
-				int LUFTransponders = simulator.getTranspondersODU(100,i, distributionType.name().toLowerCase(), 1, (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true);
-				int MUFTransponders = simulator.getTranspondersODU(100,i, distributionType.name().toLowerCase(), 2, (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true);
+				int SPFTransponders = simulator.getTranspondersODU(100,i, distributionType.name().toLowerCase(), 0, (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true, requests);
+				int LUFTransponders = simulator.getTranspondersODU(100,i, distributionType.name().toLowerCase(), 1, (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true, requests);
+				int MUFTransponders = simulator.getTranspondersODU(100,i, distributionType.name().toLowerCase(), 2, (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true, requests);
 				int optTransponders = simulator.getTransponderOPT(100,i, distributionType.name().toLowerCase(), (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true);
 				int hybridTransponders = simulator.getTranspondersHybrid(100,i, distributionType.name().toLowerCase(), (method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true, threshold);
 				int MUXTransponders = simulator.getTransponderMUX(100,i, distributionType.name().toLowerCase(),(method.equals(EMBEDDING_METHOD.WO_BACKUP))?false:true);
