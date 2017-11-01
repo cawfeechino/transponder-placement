ec544d40500c929aa968c95a0f41c6cc15cdaca9
Y Hoang <yhoang001@yahoo.com>
2017-11-01 11:06:44 -0700

    initial file

--- src/models/mainTest.java
+++ src/models/mainTest.java
@@ -1,4 +1,24 @@
 package models;
 
+import metrics.TransponderMetric;
+import utilities.NetworkTopology;
+import com.lynden.gmapsfx.javascript.object.LatLong;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
 public class mainTest {
+
+    public static void main(String args[]) throws IOException {
+
+        LatLong Intlocation = new LatLong(2.3,3.6);
+        LatLong Endlocation = new LatLong(3.7,5.4);
+
+        ArrayList<>
+
+
+
+    }
 }
+
