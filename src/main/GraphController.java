cde2a3aaf5de9c719dffadd89dfb2fe3da7b09d5
aokita <shiamatsu@gmail.com>
2017-11-07 22:51:18 -0800

    added results popup, among other things

---
+++ src/main/GraphController.java
@@ -0,0 +1,105 @@
+package main;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.text.Text;
+
+public class GraphController implements Initializable {
+	private StringBuilder resultsText = new StringBuilder();
+	private Scanner scanner;
+	
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+    
+    @FXML
+    private Text results;
+    
+    @FXML
+    private LineChart<?,?> resultsGraph;
+    
+    @FXML
+    private NumberAxis x;
+
+    @FXML
+    private NumberAxis y;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+		try {
+			String path = new File(".").getCanonicalPath().concat("/src/results/");
+			File mostRecentFile = getMostRecentFile(new File(path).listFiles());
+			setResultsData(mostRecentFile);
+			setResultsGraph(mostRecentFile);
+		} catch (Exception e) {
+		}
+    }
+    
+    private File getMostRecentFile(File[] csvFiles) {
+		File mostRecent = csvFiles[0];
+		if(csvFiles.length==1) 
+			return mostRecent;
+		
+		else {
+			
+			for(int i =0; i < csvFiles.length; i++) {
+				if(mostRecent.lastModified() < csvFiles[i].lastModified()) {
+					mostRecent = csvFiles[i];
+				}
+			}
+		}
+		return mostRecent;
+	}
+	
+	private ArrayList<String[]> readableFile(File read) throws FileNotFoundException {
+		scanner = new Scanner(read);
+		ArrayList<String[]> file = new ArrayList<>();
+		while(scanner.hasNext()) {
+			
+			String[] line = scanner.nextLine().split(",");
+			file.add(line);
+		}	
+		return file;
+	}
+	
+	private void setResultsData(File file) throws IOException {
+		ArrayList<String[]> fileArray = readableFile(file);
+		for(int i =0; i<fileArray.size();i++) {
+			for(int j=0; j< fileArray.get(i).length; j++) {
+				if(i==0)
+					resultsText.append(fileArray.get(i)[j] + "	");
+				else 
+					resultsText.append(fileArray.get(i)[j] + "				");
+			}
+			resultsText.append("\n");
+		}
+		results.setText(resultsText.toString());
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private void setResultsGraph(File file) throws IOException {
+		ArrayList<String[]> fileArray = readableFile(file);
+		x.setLabel("Bandwidth");
+		y.setLabel("Frequency");
+		XYChart.Series series = new XYChart.Series();
+		series.setName(fileArray.get(0)[1]);
+		for(int i = 1; i < fileArray.size(); i++) {
+			series.getData().add(new XYChart.Data(Integer.parseInt(fileArray.get(i)[0]), Integer.parseInt(fileArray.get(i)[1])));
+			
+		}
+		resultsGraph.getData().addAll(series);
+	}
+}
--- src/main/Gui3Controller.java
+++ src/main/Gui3Controller.java
@@ -5,21 +5,38 @@ import com.lynden.gmapsfx.MapComponentInitializedListener;
 import com.lynden.gmapsfx.javascript.object.*;
 import com.lynden.gmapsfx.service.directions.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import metrics.TransponderMetric;
 
 public class Gui3Controller implements Initializable, MapComponentInitializedListener {
-	
-	ObservableList<String> trafficMethodList = FXCollections.observableArrayList("-select a traffic request method--", "random", "gaussian", "uniform");
-	ObservableList<String> routingMethodList = FXCollections.observableArrayList("--select a routing method--", "SPF", "LUF", "MUF", "OPT", "MUX", "Hybrid");
+
+	ObservableList<String> trafficMethodList = FXCollections.observableArrayList("-select a traffic request method--",
+			"random", "gaussian", "uniform");
+	ObservableList<String> routingMethodList = FXCollections.observableArrayList("--select a routing method--", "SPF",
+			"LUF", "MUF", "OPT", "MUX", "Hybrid");
 	private String trafficMethod;
 	private String routingMethod;
 	private StringBuilder console = new StringBuilder();
@@ -41,33 +58,70 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 				.mapType(MapTypeIdEnum.ROADMAP);
 		GoogleMap map = gmap.createMap(options);
 	}
-	
-    @FXML
-    private ComboBox<String> trafficMethodBox;
-    
-    @FXML
-    private void trafficMethodChoice() {
-    	if(!trafficMethodBox.getValue().toString().equals("-select a traffic request method--")) {
-    		trafficMethod = trafficMethodBox.getValue().toString();
-    	}
-    	console.append(trafficMethod + "\n");
-    	consoleText.setText(console.toString());
-    	System.out.println(trafficMethod);
-    }
 
-    @FXML
-    private ComboBox<String> routingMethodBox;
-    
-    @FXML
-    private void routingMethodChoice() {
-    	if(!routingMethodBox.getValue().toString().equals("--select a routing method--")) {
-    		routingMethod = routingMethodBox.getValue().toString();
-    	}
-    	System.out.println(routingMethod);
-    }
-    
-    @FXML
-    private Text consoleText;
+	@FXML
+	private ComboBox<String> trafficMethodBox;
+
+	@FXML
+	private void trafficMethodChoice() {
+		if (!trafficMethodBox.getValue().toString().equals("-select a traffic request method--")) {
+			trafficMethod = trafficMethodBox.getValue().toString();
+			console.append(trafficMethod + "\n");
+		}
+		consoleText.setText(console.toString());
+	}
+
+	@FXML
+	private ComboBox<String> routingMethodBox;
+
+	@FXML
+	private void routingMethodChoice() {
+		if (!routingMethodBox.getValue().toString().equals("--select a routing method--")) {
+			routingMethod = routingMethodBox.getValue().toString();
+			console.append(routingMethod + "\n");
+		}
+		consoleText.setText(console.toString());
+	}
+
+	@FXML
+	private Button runSimulator;
+
+	@FXML
+	private void runSimulator(ActionEvent event) {
+		console.append("running simulator\n");
+		consoleText.setText(console.toString());
+		Task<Void> simulator = new Task<Void>() {
+			@Override
+			protected Void call() throws Exception {
+				String[] args = { trafficMethod.toUpperCase(), routingMethod };
+				TransponderMetric.main(args);
+				return null;
+			}
+		};
+		simulator.setOnSucceeded(workerStateEvent -> {
+			console.append("simulator finished\n");
+			consoleText.setText(console.toString());
+			try {
+				FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Graph.fxml"));
+				Parent root = (Parent) fxmlLoader.load();
+				Stage stage = new Stage();
+				stage.setTitle("Simulator Results");
+				stage.setScene(new Scene(root));
+				stage.show();
+			} catch (Exception e) {
+
+			}
+		});
+		simulator.setOnFailed(workerStateEvent -> {
+			console.append("Did you set a traffic request method or routing method?\n");
+			consoleText.setText(console.toString());
+		});
+		Thread run = new Thread(simulator);
+		run.start();
+	}
+
+	@FXML
+	private Text consoleText;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -77,4 +131,5 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 		routingMethodBox.setItems(routingMethodList);
 		routingMethodBox.getSelectionModel().select(0);
 	}
+
 }
