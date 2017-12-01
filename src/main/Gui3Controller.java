9a34e788ebd9ace437fe9c04b28a54058e2093bb
dibakar102 <dibakar102@yahoo.com>
2017-11-30 17:47:28 -0800

    updating branch gui-D

--- src/main/Gui3Controller.java
+++ src/main/Gui3Controller.java
@@ -8,6 +8,7 @@ import com.lynden.gmapsfx.javascript.event.UIEventHandler;
 import com.lynden.gmapsfx.javascript.event.UIEventType;
 import com.lynden.gmapsfx.javascript.object.*;
 
+
 import com.lynden.gmapsfx.service.geocoding.GeocoderStatus;
 import com.lynden.gmapsfx.service.geocoding.GeocodingResult;
 import com.lynden.gmapsfx.service.geocoding.GeocodingService;
@@ -28,10 +29,14 @@ import java.util.Scanner;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
@@ -41,6 +46,8 @@ import javafx.scene.control.TitledPane;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import metrics.TransponderMetric;
 import models.MapNode;
 import netscape.javascript.JSObject;
 
@@ -55,7 +62,6 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 	private String routingMethod;
 	private StringBuilder console = new StringBuilder();
 
-
 	@FXML
 	private ResourceBundle resources;
 
@@ -69,13 +75,22 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 	private TableColumn<MapNode, Integer> nodes,columnNode;
 
 	@FXML
-	private TableColumn<MapNode, Double> lat_list, log_list, columnLat, columnLng;
+	private TableColumn<MapNode, Double> lat_list, log_list;
 	
 	@FXML
-	private TableColumn<LatLong, Double> start, end, distance;
+	private TableColumn<MapNode, Double> columnlat, columnlng;
+	
+	@FXML
+	private TableColumn<MapNode, LatLong> start, end;
+	
+	@FXML
+	private TableColumn<MapNode, Double> distance;
 
 	@FXML
-	private TableView<MapNode> nodeList, saveNodes, saveLinks;
+	private TableView<MapNode> nodeList, saveNodes;
+	
+	@FXML
+	private TableView<MapNode> saveLinks;
 
 	@FXML
 	private TitledPane nodeEdit;
@@ -94,8 +109,7 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 	
 	private ArrayList<LatLong> locations;
 	
-	private ArrayList<LatLong[]> connections;
-	
+	private ArrayList<MapNode> connections;
 
 	
 	@Override
@@ -122,7 +136,9 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 
 		connections = getConnections();
 		
-		for (LatLong[] pair : connections) {
+		for (MapNode links : connections) {
+			LatLong[] pair = new LatLong[] {links.getStart(), links.getEnd()};
+			
 			MVCArray mvcArray = new MVCArray(pair);
 			PolylineOptions polylineOptions = new PolylineOptions().path(mvcArray).strokeColor("black").strokeWeight(1.5);
 			Polyline polyline = new Polyline(polylineOptions);
@@ -130,6 +146,8 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 			
 		}
 		
+		
+		//add listener on table once user click on locations on table adds to the connection addition section
 		nodeList.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
 			@Override
@@ -145,26 +163,15 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 		});
 		
 		
+		
 		//adding a new point
-		map.addMouseEventHandler(map, UIEventType.rightclick, new MouseEventHandler() {
+		map.addMouseEventHandler(map, UIEventType.click, new MouseEventHandler() {
 
 			@Override
 			public void handle(GMapMouseEvent arg0) {
-
-				// TODO Auto-generated method stub
-
 				LatLong location = arg0.getLatLong();
 				Double shortest = Double.MAX_VALUE;
 				Double limit = 96560.64; //meters t0 miles(60)
-				
-				MarkerOptions markerOptions = new MarkerOptions();
-				markerOptions.position(location);
-				Marker marker = new Marker(markerOptions);
-				map.addMarker(marker);	
-				
-
-				
-				
 
 				for(LatLong point : locations){
 					if(shortest > point.distanceFrom(location)) {
@@ -174,7 +181,6 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 				if(shortest> limit) {
 					lat.setText(format.format(location.getLatitude()));
 					log.setText(format.format(location.getLongitude()));
-
 				
 					geocodingService.reverseGeocode(location.getLatitude(), location.getLongitude(), new GeocodingServiceCallback() {
 						
@@ -204,11 +210,10 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 				String nlng = format.format(Double.parseDouble(log.getText()));
 				LatLong newPoint = new LatLong(Double.parseDouble(nlat), Double.parseDouble(nlng));
 				
-
 					addMarker(newPoint, map, infoWindow, infoWindowOptions);
-
 			}
 		});
+		
 		
 		addConnection.setOnAction(new EventHandler<ActionEvent>() {
 					
@@ -218,7 +223,6 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 						boolean valid = false;
 						LatLong[] pair = new LatLong[]{new LatLong(Double.parseDouble(startNorth.getText()), Double.parseDouble(startEast.getText())),
 														new LatLong(Double.parseDouble(endNorth.getText()), Double.parseDouble(endEast.getText()))};
-						
 						
 						
 						List<MapNode> items = nodeList.getItems();
@@ -270,16 +274,21 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 						}
 					}
 				});
-	}//end of mapInitialized method
+	}
 
 	@FXML
 	public void displaySave() {
 	
 		saveNodes.getItems().addAll(nodeList.getItems());
 		columnNode.setCellValueFactory(new PropertyValueFactory<>("id"));
-		columnLat.setCellValueFactory(new PropertyValueFactory<>("latitude"));
-		columnLng.setCellValueFactory(new PropertyValueFactory<>("longitude"));
-		
+		columnlat.setCellValueFactory(new PropertyValueFactory<>("latitude"));
+		columnlng.setCellValueFactory(new PropertyValueFactory<>("longitude"));
+	
+		saveLinks.getItems().addAll(connections);
+		start.setCellValueFactory(new PropertyValueFactory<>("start"));
+		end.setCellValueFactory(new PropertyValueFactory<>("end"));
+		distance.setCellValueFactory(new PropertyValueFactory<>("distance"));
+
 	}
 	
 	@FXML
@@ -309,6 +318,43 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 			routingMethod = routingMethodBox.getValue().toString();
 		}
 		System.out.println(routingMethod);
+	}
+	
+	@FXML
+	private Button runSimulator;
+
+	@FXML
+	private void runSimulator(ActionEvent event) {
+		console.append("running simulator\n");
+		consoleText.setText(console.toString());
+		Task<Void> simulator = new Task<Void>() {
+			@Override
+			protected Void call() throws Exception {
+				String[] args = { trafficMethod.toUpperCase(), routingMethod };
+				TransponderMetric.main(args);
+				return null;
+			}
+		};
+		simulator.setOnSucceeded(workerStateEvent -> {
+			console.append("simulator finished\n");
+			consoleText.setText(console.toString());
+			try {
+				FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Graph.fxml"));
+				Parent root = (Parent) fxmlLoader.load();
+				Stage stage = new Stage();
+				stage.setTitle("Simulator Results");
+				stage.setScene(new Scene(root));
+				stage.show();
+			} catch (Exception e) {
+
+			}
+		});
+		simulator.setOnFailed(workerStateEvent -> {
+			console.append("Did you set a traffic request method or routing method?\n");
+			consoleText.setText(console.toString());
+		});
+		Thread run = new Thread(simulator);
+		run.start();
 	}
 
 	@FXML
@@ -345,8 +391,8 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 	}
 	
 	
-	public ArrayList<LatLong[]> getConnections(){
-		ArrayList<LatLong[]> connections = new ArrayList<>();
+	public ArrayList<MapNode> getConnections(){
+		ArrayList<MapNode> connections = new ArrayList<>();
 		String path;
 		try {
 			path = new File(".").getCanonicalPath().concat("/src/assets/locationConnections.txt");
@@ -359,8 +405,9 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 				String[] f = line[0].split(",");
 				String[] s = line[1].split(",");
 				double distance = Double.parseDouble(line[2]);
-				LatLong[] pairs = new LatLong[]{new LatLong(Double.parseDouble(f[0]),Double.parseDouble(f[1])),new LatLong(Double.parseDouble(s[0]),Double.parseDouble(s[1]))}; 
-				connections.add(pairs);
+				LatLong start = new LatLong(Double.parseDouble(f[0]),Double.parseDouble(f[1]));
+				LatLong end = new LatLong(Double.parseDouble(s[0]),Double.parseDouble(s[1]));
+				connections.add(new MapNode(start, end, distance));
 			}	
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -407,6 +454,7 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 		Marker marker = new Marker(markerOptions);
 		map.addMarker(marker);
 		
+		
 		map.addUIEventHandler(marker, UIEventType.click, new UIEventHandler() {
 			
 			@Override
@@ -425,10 +473,8 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 						infoWindowOptions.content(display);
 						infoWindow.setOptions(infoWindowOptions);
 						infoWindow.open(map,marker);	
-						
 					}
 				});	
-			
 			}
 		});
 		
@@ -439,6 +485,7 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 				LatLong ll = new LatLong((JSObject) arg0.getMember("latLng"));
 				//TODO check if user inputs location manually that will ruin code
 				
+				//TODO add to connections list
 				
 				//first user double click to set up connection to second dbclick
 				if(startNorth.getText().equals("")&startEast.getText().equals("")) {
@@ -468,129 +515,5 @@ public class Gui3Controller implements Initializable, MapComponentInitializedLis
 		nodes.setCellValueFactory(new PropertyValueFactory<>("id"));
 		lat_list.setCellValueFactory(new PropertyValueFactory<>("latitude"));
 		log_list.setCellValueFactory(new PropertyValueFactory<>("longitude"));
-	}
-	
-	
-	//default locations and pairs for nsfnet
-	
-	//i dont use these methods but i dont want to delete it just in case the file get deleted or somthing so i have this as back up
-	public ArrayList<LatLong> locations() {
-		LatLong sd = new LatLong(32.7157, -117.1611);
-		LatLong palo = new LatLong(37.4419, -122.143);
-		LatLong seattle = new LatLong(47.6062, -122.3321);
-		LatLong slc = new LatLong(40.7608, -111.8910);
-		LatLong boulder = new LatLong(40.0150, -105.2705);
-		LatLong houston = new LatLong(29.7604, -95.3698);
-		LatLong lincoln = new LatLong(40.8258, -96.6852);
-		LatLong champaign = new LatLong(40.1164, -88.2434);
-		LatLong atlanta = new LatLong(33.749, -84.388);
-		LatLong pittsburgh = new LatLong(40.4406, -79.9959);
-		LatLong annArbor = new LatLong(42.2808, -83.7430);
-		LatLong ithaca = new LatLong(43.444, -76.5019);
-		LatLong collegePk = new LatLong(38.9897, -76.9378);
-		LatLong princeton = new LatLong(40.3487, -74.659);
-		
-
-		ArrayList<LatLong> locations = new ArrayList<>();
-		locations.add(sd);
-		locations.add(palo);
-		locations.add(seattle);
-		locations.add(slc);
-		locations.add(boulder);
-		locations.add(houston);
-		locations.add(lincoln);
-		locations.add(champaign);
-		locations.add(atlanta);
-		locations.add(pittsburgh);
-		locations.add(annArbor);
-		locations.add(ithaca);
-		locations.add(collegePk);
-		locations.add(princeton);
-		return locations;
-	}
-	
-	
-	public ArrayList<LatLong[]> locationConnection() {
-		LatLong sd = new LatLong(32.7157, -117.1611);
-		LatLong palo = new LatLong(37.4419, -122.143);
-		LatLong seattle = new LatLong(47.6062, -122.3321);
-		LatLong slc = new LatLong(40.7608, -111.8910);
-		LatLong boulder = new LatLong(40.0150, -105.2705);
-		LatLong houston = new LatLong(29.7604, -95.3698);
-		LatLong lincoln = new LatLong(40.8258, -96.6852);
-		LatLong champaign = new LatLong(40.1164, -88.2434);
-		LatLong atlanta = new LatLong(33.749, -84.388);
-		LatLong pittsburgh = new LatLong(40.4406, -79.9959);
-		LatLong annArbor = new LatLong(42.2808, -83.7430);
-		LatLong ithaca = new LatLong(43.444, -76.5019);
-		LatLong collegePk = new LatLong(38.9897, -76.9378);
-		LatLong princeton = new LatLong(40.3487, -74.659);
-
-		
-		ArrayList<LatLong[]> connections = new ArrayList<>();
-		LatLong[] sdANDpalo = new LatLong[] { sd, palo };
-		connections.add(sdANDpalo);
-
-		LatLong[] sdANDsea = new LatLong[] { sd, seattle };
-		connections.add(sdANDsea);
-
-		LatLong[] paloANDsea = new LatLong[] { seattle, palo };
-		connections.add(paloANDsea);
-
-		LatLong[] paloANDslc = new LatLong[] { slc, palo };
-		connections.add(paloANDslc);
-
-		LatLong[] seattleANDchampaign = new LatLong[] { seattle, champaign };
-		connections.add(seattleANDchampaign);
-
-		LatLong[] sdANDhouston = new LatLong[] { sd, houston };
-		connections.add(sdANDhouston);
-
-		LatLong[] slcANDboulder = new LatLong[] { slc, boulder };
-		connections.add(slcANDboulder);
-
-		LatLong[] slcANDannArbor = new LatLong[] { slc, annArbor };
-		connections.add(slcANDannArbor);
-
-		LatLong[] boulderANDhouston = new LatLong[] { boulder, houston };
-		connections.add(boulderANDhouston);
-
-		LatLong[] boulderANDlincoln = new LatLong[] { boulder, lincoln };
-		connections.add(boulderANDlincoln);
-
-		LatLong[] lincolnANDchampaign = new LatLong[] { lincoln, champaign };
-		connections.add(lincolnANDchampaign);
-
-		LatLong[] champaignANDpittsburgh = new LatLong[] { champaign, pittsburgh };
-		connections.add(champaignANDpittsburgh);
-
-		LatLong[] houstonANDatlnta = new LatLong[] { houston, atlanta };
-		connections.add(houstonANDatlnta);
-
-		LatLong[] houstonANDcollegePk = new LatLong[] { houston, collegePk };
-		connections.add(houstonANDcollegePk);
-
-		LatLong[] atlantaANDpittsburgh = new LatLong[] { atlanta, pittsburgh };
-		connections.add(atlantaANDpittsburgh);
-
-		LatLong[] annArborANDprinceton = new LatLong[] { annArbor, princeton };
-		connections.add(annArborANDprinceton);
-
-		LatLong[] annArborANDithica = new LatLong[] { annArbor, ithaca };
-		connections.add(annArborANDithica);
-
-		LatLong[] pittsburghANDithaca = new LatLong[] { pittsburgh, ithaca };
-		connections.add(pittsburghANDithaca);
-
-		LatLong[] pittsburghANDprinceton = new LatLong[] { pittsburgh, princeton };
-		connections.add(pittsburghANDprinceton);
-
-		LatLong[] collegePkANDithaca = new LatLong[] { collegePk, ithaca };
-		connections.add(collegePkANDithaca);
-
-		LatLong[] collegePkANDprinceton = new LatLong[] { collegePk, princeton };
-		connections.add(collegePkANDprinceton);
-		
-		return connections;
 	}
 }
